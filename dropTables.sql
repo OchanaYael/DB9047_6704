80e78635532afa2820a8d9349548b060e358f990
chaniSSSSSSS <125955082+chaniSSSSSSS@users.noreply.github.com>
2024-05-28 22:17:12 +0300

    Add files via upload
    uploaded files

---
+++ dropTables.sql
@@ -0,0 +1,11 @@
+DROP TABLE  madicinshift;
+DROP TABLE  driverinshift;
+DROP TABLE  drivertoambu;
+DROP TABLE  ambutoevent;
+DROP TABLE  Shift;
+DROP TABLE  Paramedic;
+DROP TABLE  Driver;
+DROP TABLE  Medic;
+DROP TABLE  Volunteer;
+DROP TABLE  Event;
+DROP TABLE  ambulans;
